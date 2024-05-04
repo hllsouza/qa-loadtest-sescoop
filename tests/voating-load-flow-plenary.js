6df336953dcebc33afdb2deed2c13ac977a71b5c
hllsouza <bugatoo@gmail.com>
2024-05-04 11:58:58 -0300

    update url hml

--- tests/voating-load-flow-plenary.js
+++ tests/voating-load-flow-plenary.js
@@ -13,13 +13,13 @@ const userData = new SharedArray("userCredentials", function () {
 export const options = {
   // Key configurations for Stress in this section
   stages: [
-    { duration: "5m", target: 2000 },
     { duration: "2m", target: 2000 },
+    { duration: "5m", target: 2000 },
     { duration: "2m", target: 3000 },
     { duration: "5m", target: 3000 },
     { duration: "2m", target: 4000 },
     { duration: "5m", target: 4000 },
-    { duration: "5m", target: 0 },
+    { duration: "5s", target: 0 },
   ],
   thresholds: {
     http_req_duration: ["p(95)<2000"], //95% das reqs devem responder em até 2s
@@ -33,7 +33,7 @@ export const options = {
   },
 };
 
-export default async function () {
+export default function () {
   let user = userData[__VU % userData.length];
   const atividadeIds = [26];
   const atividadeId = randomItem(atividadeIds);
