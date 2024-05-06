94644b9a8dad1ecc12651f685bdcbc8d2ed32acc
hllsouza <bugatoo@gmail.com>
2024-05-06 16:48:42 -0300

    fix endpoint plenary_vote

--- requests/plenary_vote.js
+++ requests/plenary_vote.js
@@ -3,11 +3,11 @@ import http from "k6/http";
 import Utils from "../utils/utils.js";
 
 export default class Plenary {
-  async plenaryVote(token, pauta, participant) {
+  async plenaryVote(token, participant) {
     const url = `${Utils.getBaseUrl()}/atividades/atividades/votar`;
     const payload = JSON.stringify({
       tipo: "plenaria",
-      pauta_id: pauta,
+      pauta_id: 97,
       participante_id: participant,
       propostas: [284],
       urgencia: 4,
@@ -19,7 +19,6 @@ export default class Plenary {
         Authorization: `Bearer ${token}`,
       },
     };
-
     const response = await http.asyncRequest('POST', url, payload, params);
     check(response, {
       "is status 200": () => response.status === 200,
